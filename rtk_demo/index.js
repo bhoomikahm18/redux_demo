a789c98356cfd7a8f86dc9d157185fa3265ff000
Bhoomika <bhoomikahm18@gmail.com>
2023-12-29 14:56:10 +0530

    Configuring store

---
+++ rtk_demo/index.js
@@ -0,0 +1,14 @@
+const store = require("./app/store.js");
+const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state", store.getState());
+});
+
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.ordered());
+store.dispatch(cakeActions.restocked(3));
+
+unsubscribe();
